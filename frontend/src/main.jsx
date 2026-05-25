aa26e48e81dd519cefc50bc922a932a0f98466e0
Casper <shibby776@gmail.com>
2026-05-25 17:24:57 +1000

    Removed the Password Page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import SitePasswordGate from "./components/SitePasswordGate.jsx";
 import "./shared.css";
 import "./interview-prep-builder.css";
 import "./profile-app.css";
@@ -9,8 +8,6 @@ import "./jobinput.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SitePasswordGate>
-      <App />
-    </SitePasswordGate>
+    <App />
   </React.StrictMode>
 );
